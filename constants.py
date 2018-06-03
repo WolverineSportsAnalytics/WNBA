487db1289bbde02d86e6b4dd9749ff49c097924f
Your Name <evanciancio@evans-MacBook-Pro.local>
2018-06-02 21:28:00 -0400

    Add constants and symlink for constants in tests

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 # Plan on having the constants file in the repo this time
 # We will however have placeholders for the passwords 
 # Passwords can be retrieved from a credentials file in Google Drive
+testHost='127.0.0.1'               
+testUser='root'                                    
+testName='wnba_test'                                           
+testPassword=''       
