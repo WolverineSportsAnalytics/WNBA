b7ebbf283bc1054e7977ced967145b46669604eb
Evan Ciancio <eciancio@llnw.com>
2018-06-27 09:58:44 -0400

    Scheduler Job so we don't need to configure cron

---
+++ automationScheduler.py
@@ -0,0 +1,21 @@
+import time
+import datetime
+import automation
+from apscheduler.schedulers.background import BackgroundScheduler
+def test_scheduler():
+    print "Runing"
+
+test_scheduler()
+scheduler = BackgroundScheduler()
+
+scheduler.start()
+
+scheduler.add_job(automation.main, 'cron', day_of_week='mon-fri', hour=15, minute=30, timezone='US/Eastern') 
+scheduler.add_job(automation.main, 'cron', day_of_week='sat-sun', hour=12, minute=30, timezone='US/Eastern') 
+
+try:
+    while True:
+        time.sleep(2)
+except (KeyboardInterrupt, SystemExit):
+    # Not strictly necessary if daemonic mode is enabled but should be done if possible
+    scheduler.shutdown()
